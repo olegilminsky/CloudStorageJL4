d7421b10b0bf0fa97c1bb2bc47d441c3871075b7
olegilminsky <olegilminsky@yandex.ru>
2021-05-20 17:25:49 +0300

    HomeWork 1 JavaLevel4

--- src/main/java/com/ilminsky/server/Server.java
+++ src/main/java/com/ilminsky/server/Server.java
@@ -1,16 +1,25 @@
 package com.ilminsky.server;
 
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
     public Server() {
         ExecutorService service = Executors.newFixedThreadPool(4);
-        try (ServerSocket server = new ServerSocket(9876)){
-            server.accept();
-        }catch (Exception e){
+        try (ServerSocket server = new ServerSocket(9876)) {
+            while (true) {
+                Socket socket = server.accept();
+                System.out.println("Client accepted - " + socket.getInetAddress());
+                service.execute(new ClientHandler(server.accept()));
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        new Server();
     }
 }
